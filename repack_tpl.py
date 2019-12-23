ed873db59b951aefbd18b4b52e64031391d5115c
Illidanz <illidanrulez@gmail.com>
2019-12-23 10:52:03 +0100

    Fixed TPL repack

--- repack_tpl.py
+++ repack_tpl.py
@@ -10,21 +10,32 @@ def run():
     repackfolder = "data/repack_TPL/"
     common.makeFolder(repackfolder)
 
+    common.logMessage("Copying original TPL files...")
+    common.copyFolder(outfolderarc.replace("repack/", "extract/"), outfolderarc)
+    common.copyFolder(outfolder.replace("repack/", "extract/") + "textures", outfolder + "textures")
     common.logMessage("Repacking TPL from", workfolder, "...")
     files = common.getFiles(workfolder, ".png")
     for file in common.showProgress(files):
         common.logDebug("Processing", file, "...")
         archive = file.split("/")[0]
         folder = outfolder
+        format = ""
         if archive != "textures":
             folder = repackfolder
             if not os.path.isdir(folder + archive):
                 common.copyFolder(extractfolder + archive, repackfolder + archive)
             archive += "/root/timg"
-        common.execute("wimgt -o ENCODE " + workfolder + file + " -D " + folder + archive + "/" + os.path.basename(file).replace(".png", ".tpl"), False)
+            format = " -x TPL.C8.P3"
+        elif "mm1" in file:
+            continue
+        common.execute("wimgt -o ENCODE " + workfolder + file + " -D " + folder + archive + "/" + os.path.basename(file).replace(".png", ".tpl") + format, False)
 
     common.logMessage("Repacking ARC from", repackfolder, "...")
     files = os.listdir(repackfolder)
     for file in common.showProgress(files):
         common.logDebug("Processing", file, "...")
         common.execute("wszst -o CREATE " + repackfolder + file + " -D " + outfolderarc + file, False)
+        # Blank out the 0xCC bytes that are different from the original
+        with common.Stream(outfolderarc + file, "r+b") as f:
+            f.seek(16)
+            f.writeZero(16)
