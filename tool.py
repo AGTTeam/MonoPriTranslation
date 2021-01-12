a1934323cbb6f7a753cf4ab66e3ab5151851cac5
Illidan <illidanrulez@gmail.com>
2021-01-12 17:18:22 +0100

    Bump hacktools

--- tool.py
+++ tool.py
@@ -8,7 +8,7 @@ import pyimgur
 import requests
 from hacktools import common, wii
 
-version = "1.3.7"
+version = "1.3.8"
 isofile = "data/disc.iso"
 infolder = "data/extract/"
 outfolder = "data/repack/"
